1c46fff11ea65c75e62900d6615daf73a29bbd89
yoon-0012 <dbzho8383@kpu.ac.kr>
2022-10-14 04:37:36 +0900

    처음부터 다시 시작

--- 지형 텍스쳐링2/LabProject08-2-0/Scene.h
+++ 지형 텍스쳐링2/LabProject08-2-0/Scene.h
@@ -77,7 +77,7 @@ protected:
 public:
 	float						m_fElapsedTime = 0.0f;
 	std::vector<CGameModelObj*> objBox;
-	std::vector<char*> heli{ "Models/Gunship.bin", "Models/SuperCobra.bin", "Models/Mi24.bin", "Models/Apache.bin" };
+	std::vector<char*> heli{ "Models/Gunship.bin", "Models/SuperCobra.bin", "Models/Mi24.bin", "Models/Player.bin" };
 
 	ID3D12Resource* m_pd3dcbLights = NULL;
 	LIGHTS* m_pcbMappedLights = NULL;
